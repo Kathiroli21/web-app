9dac8c84d55a96093222f8b9a455269cecf2b6b7
Kathiroli21 <161636321+Kathiroli21@users.noreply.github.com>
2024-07-19 09:10:18 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -20,3 +20,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
+ 
